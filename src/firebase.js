f7854f15251d4c9d39a3021e7fb28eaa8aafe9d8
Jack Blanc <jackblanc@Jacks-MBP.fios-router.home>
2020-04-07 05:12:44 -0400

    update firebase npm reference

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/database";
 
 var config = {
   apiKey: "AIzaSyCYCXB4wt7cz1Zb9rjjHeev0o1RSoNkfAU",
